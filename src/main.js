6346f3034d0ccd8b84785f89a7bf1d726ec0cc14
ZZQ <18736271057@163.com>
2020-05-08 22:39:06 +0800

    v001   页面访问控制

--- src/main.js
+++ src/main.js
@@ -23,8 +23,19 @@ const i18n = new VueI18n({
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
     document.title = `${to.meta.title} | vue-manage-system`;
-    const role = localStorage.getItem('ms_username');
-    if (!role && to.path !== '/login') {
+    const username = localStorage.getItem('ms_username');
+    const role = localStorage.getItem('ms_role');
+    if (to.path === '/student_mgr' || to.path === '/admin_mgr' || to.path === '/teacher_mgr'){
+        if(role === '1'){
+            next()
+        } else {
+            Vue.prototype.$alert('无管理员权限！','通知', {
+                confirmButtonText: '确定'
+            });
+            this.$router.go(-1);
+        }
+    }
+    if (!username && to.path !== '/login') {
         next('/login');
     } else if (to.meta.permission) {
         // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
